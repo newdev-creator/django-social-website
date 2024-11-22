ed095084306df94fdb35ffc9ba3e91150c5e2e58
Julien <newdevcreator@gmail.com>
2024-11-22 21:44:06 +0100

    FEAT: simple auth

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+from .form import LoginForm
 
-# Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authentificated successfully')
+                else:
+                    return HttpResponse('Disable account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(
+        request,
+        'account/login.html',
+        {'form': form})
