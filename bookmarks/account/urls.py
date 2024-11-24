dd63538b687250d074e95b3c7f6dca46f568fd5a
Julien <newdevcreator@gmail.com>
2024-11-24 21:37:34 +0100

    FEAT: create the forgotten password methode

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -22,6 +22,28 @@ urlpatterns = [
         name='password_change_done'
     ),
 
+    # Reset password urls
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'  
+    ),
+    path(
+        'password-reset/<uidb64>/<token>',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password-reset/complete',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
+
     # site urls
     path('', views.dashboard, name='dashboard'),
 ]   
