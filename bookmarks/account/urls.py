c261e16c56a09758c90fbcb5664b82ce4d840a9e
Julien <newdevcreator@gmail.com>
2024-11-24 11:28:59 +0100

    FEAT: add route for passeword change

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,8 +5,18 @@ from . import views
 urlpatterns = [
     # previous login
     # path('login/', views.user_login, name='login'),
+
     # login / logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # change password urls
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+
+    # site urls
     path('dashboard/', views.dashboard, name='dashboard'),
 ]   
