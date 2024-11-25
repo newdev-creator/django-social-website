97151bc405134a84e59d9c283d97486f0259f08e
Julien <newdevcreator@gmail.com>
2024-11-25 20:21:45 +0100

    FEAT: create register

--- bookmarks/account/form.py
+++ bookmarks/account/form.py
@@ -1,6 +1,30 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name')
+
+    def clean_password2(self: forms.Form) -> str:
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match')
+        return cd['password2']
