74457b1ea7f32dfd4afd2957a18e43fa72c5d50c
Julien <newdevcreator@gmail.com>
2024-11-27 20:58:48 +0100

    FEAT: messages

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,5 @@
+from email import messages
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
@@ -83,6 +85,12 @@ def edit(request: HttpRequest) -> HttpResponse:
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request,
+                'Profile updated successfully'
+            )
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
